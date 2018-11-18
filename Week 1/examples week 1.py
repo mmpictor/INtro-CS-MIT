dfdc4646229b164e5c3c8f89105973bf9b3099cd
Micaela <mpictorgarcia@gmail.com>
2018-11-17 23:40:10 -0500

    Create examples week 1.py
    examples week 1

---
+++ Week 1/examples week 1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 17 23:34:34 2018
+
+@author: MASTER
+"""
+
+# -*- coding: utf-8 -*-
+
+#x = int (input('enter an integer: '))
+#
+#if x % 2 == 0:
+#    print ('')
+#    print ('Even')
+#else:
+#    print ('')
+#    print ('odd')
+#print ('done with conditional')
+
+#x =float (input("enter a number for x: "))
+#y =float (input("enter a number for y: "))
+#if x == y:
+#    print ("x and y are equal")
+#    if y!= 0 :
+#        print ("therefore, x / y is", x/y)
+#elif x < y :
+#    print ("x is smaller")
+#else:
+#    print ("y is smaller")
+#print ("thanks!!")
+#n=5
+#if n>2:
+#    print (n)
+#    n-=1
+#print (n)
+#print ('hello')
+#n=10
+#while n>0:
+#    print(n)
+#n-=2
+#iteration = 0
+#count = 0
+#while iteration < 5:
+#    for letter in "hello, world":
+#        count += 1
+#    print("Iteration " + str(iteration) + "; count is: " + str(count))
+#    iteration += 2
